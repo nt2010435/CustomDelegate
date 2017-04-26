e911f05d0dafee83805c70818aadde9acb9b70c3
NT <nt2010435@outlook.com>
2017-04-26 08:01:09 -0700

    Added raise events

--- DelegatesAndEvents/Worker.cs
+++ DelegatesAndEvents/Worker.cs
@@ -10,12 +10,46 @@ namespace DelegatesAndEvents
 
     public class Worker
     {
-        public event WorkPerformedHandler WorkPerformed;
+        public event WorkPerformedHandler WorkPerformed; //Event definition
         public event EventHandler WorkCompleted;
 
         public void DoWork(int hours, WorkType workType)
         {
+            for (int i = 0; i < hours; i++)
+            {
+                //Raise event
+                OnWorkPerformed(i + 1, workType);
+            }
+            //Raise event
+            OnWorkCompleted();
+        }
 
+        protected virtual void OnWorkPerformed(int hours, WorkType workType)
+        {
+            //if(WorkPerformed != null)
+            //{
+            //    WorkPerformed(hours, workType);
+            //}
+
+            var del = WorkPerformed as WorkPerformedHandler;
+            if (del != null)
+            {
+                del(hours, workType);
+            }
+        }
+
+        protected virtual void OnWorkCompleted()
+        {
+            //if(WorkCompleted != null)
+            //{
+            //    WorkCompleted(this, EventArgs.Empty);
+            //}
+
+            var del = WorkCompleted as EventHandler;
+            if (del != null)
+            {
+                del(this, EventArgs.Empty);
+            }
         }
     }
 }
