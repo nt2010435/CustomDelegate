9db4abace17de1630e42d8412ffb67285c02bda0
NT <nt2010435@outlook.com>
2017-04-27 09:49:14 -0700

    Added lambdas to code

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -12,72 +12,20 @@ namespace DelegatesAndEvents
 
         static void Main(string[] args)
         {
-            //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
-            //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
-            //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
-
-            //del1(5, WorkType.Golf);
-            //del2(10, WorkType.GenerateReports);
-            //DoWork(del1);
-
-            /* Multicast delegates */
-            //del1 += del2;
-            //del1 += del3;
-            //del1 += del2 + del3;
-
-            /* Last delegate returns the value for this block of code*/
-            //int finalHours = del1(10, WorkType.GenerateReports);
-            //Console.WriteLine(finalHours);
-
             var worker = new Worker();
-            //Anonymous method
-            worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
+            //Lambdas (inline method, parameters, body)
+            worker.WorkPerformed += (s, e) =>
             {
                 Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+                Console.WriteLine("Good progress...");
             };
-
-            worker.WorkCompleted += delegate (object sender, EventArgs e)
-            {
-                Console.WriteLine("Worker is done!");
-            };
+            worker.WorkCompleted += (s,e) => Console.WriteLine("Worker is done!");
 
             worker.DoWork(8, WorkType.GenerateReports);
 
             Console.Read();
         }
 
-        //private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        //{
-        //    Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
-        //}
-
-        //static void Worker_WorkCompleted(object sender, EventArgs e)
-        //{
-        //    Console.WriteLine("Worker is done!");
-        //}
-
-        //static void DoWork(WorkPerformedHandler del)
-        //{
-        //    del(5, WorkType.GoToMeetings);
-        //}
-
-        //static int WorkPerformed1(int hours, WorkType workType)
-        //{
-        //    Console.WriteLine("WorkPerformed1 called " + hours.ToString());
-        //    return hours + 1;
-        //}
-
-        //static int WorkPerformed2(int hours, WorkType workType)
-        //{
-        //    Console.WriteLine("WorkPerformed2 called " + hours.ToString());
-        //    return hours + 2;
-        //}
-
-        //static int WorkPerformed3(int hours, WorkType workType)
-        //{
-        //    Console.WriteLine("WorkPerformed3 called " + hours.ToString());
-        //    return hours + 3;
-        //}
     }
 
     public enum WorkType
