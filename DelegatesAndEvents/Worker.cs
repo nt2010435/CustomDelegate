8c8f6a381b2ff1fe8f702a4d3d352c23482faf6a
NT <nt2010435@outlook.com>
2017-04-27 08:08:58 -0700

    Added instantiating delegates & handling events

--- DelegatesAndEvents/Worker.cs
+++ DelegatesAndEvents/Worker.cs
@@ -18,6 +18,7 @@ namespace DelegatesAndEvents
         {
             for (int i = 0; i < hours; i++)
             {
+                System.Threading.Thread.Sleep(1000);
                 //Raise event
                 OnWorkPerformed(i + 1, workType);
             }
